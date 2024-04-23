76946e108ceb0e81ce00b07deecb452fdec3b390
Sia Khorsand <skhorsand@ucsd.edu>
2024-04-23 14:22:52 -0700

    added new tests and debugged ArrayExamples.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+@Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 3, 7, 8 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 8, 7, 3}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverse() {
+    int[] input1 = {2, 5, 7, 9, 13};
+    assertArrayEquals(new int[]{13, 9, 7, 5, 2}, ArrayExamples.reversed(input1));
   }
 }
